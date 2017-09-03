056b9a07a0721e6ce82da9383e9da20eef1c22ac
Ludwig Gustavsson <ludwiggustavsson3@gmail.com>
2017-09-03 15:09:51 +0200

    Added scene, added basic stuff

---
+++ BallPhysics/Assets/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using PhysicsAssignments.Constants;
+
+namespace PhysicsAssignments.Object
+{
+    public class Ball : MonoBehaviour
+    {
+        #region Private exposed fields
+
+        [Header("Mass (KG)")]
+        [Range(0.0f, 10000.0f)]
+        [SerializeField]
+        float m_Mass = 10.0f;
+
+        #endregion
+
+        #region Private fields
+
+        //Position & velocity
+        Vector2 m_Position;
+        Vector3 m_Velocity;
+
+        #endregion
+
+        void FixedUpdate()
+        {
+            if (transform.position.y < 0.0f) m_Velocity *= -0.5f;
+            else m_Velocity += m_Mass * Constants.Constants.GRAVITY / 100.0f * Time.fixedDeltaTime;
+
+            transform.position += m_Velocity;
+        }
+    }
+}
---
+++ BallPhysics/Assets/Scripts/Constants.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace PhysicsAssignments.Constants
+{
+    /// <summary>
+    /// Class containing physics-related constant values
+    /// </summary>
+    public class Constants : MonoBehaviour
+    {
+        public static Vector3 GRAVITY = new Vector3(0.0f, -9.82f, 0.0f);
+    }
+}
