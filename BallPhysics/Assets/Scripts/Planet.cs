e69ea85c15822997b61b741dd647f8ddd910aa14
Ludwig Gustavsson <ludwiggustavsson3@gmail.com>
2017-10-03 16:51:37 +0200

    Orbit

--- BallPhysics/Assets/Scripts/Planet.cs
+++ BallPhysics/Assets/Scripts/Planet.cs
@@ -9,11 +9,27 @@ public class Planet : MonoBehaviour {
     
     private bool m_gravity = false;
 
+    [SerializeField]
+    Planet m_GravitationalSource;
+
+    [SerializeField]
+    bool m_Moon = false;
+
     void Awake () {
         m_body = new Body(transform.position, Vector3.zero, Vector3.zero);
     }
 	
 	void FixedUpdate () {
+        Vector3 diff = m_GravitationalSource.transform.position - transform.position;
+        Vector3 dir = diff.normalized;
+        float force = ( m_GravitationalSource.GetMass () * GetMass () * -9.82f ) / diff.sqrMagnitude;
+        AddForce (dir * force);
+
+        if (m_Moon)
+        {
+            AddForce (Quaternion.AngleAxis (90.0f, Vector3.up) * dir * 100f);
+        }
+
         transform.position += m_body.Velocity * Time.fixedDeltaTime;
         m_body.Velocity *= 0.9f;
     }
